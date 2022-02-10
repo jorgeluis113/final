1d00d26045c189e48d201f863b85fe4e0e18a172
Jorge Luis <jluissanz@iesmordefuentes.com>
2022-02-10 20:23:45 +0100

    eee

--- sube.sh
+++ sube.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
+ git push https://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@github.com/jorgeluis113/final.git
 
-sudo git push https://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@github.com/jorgeluis113/final.git
+git push -u origin main
